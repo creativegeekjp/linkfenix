e96d422d7a8b77bdd8e591b15eef68b39f8aee45
dev003 <dev003@dev003.com>
2016-03-31 08:42:26 +0000

    activation trial

--- wp-config.php
+++ wp-config.php
@@ -94,7 +94,7 @@ require_once(ABSPATH . 'wp-settings.php');
 // get already activated plugins
 $plugins = get_option('active_plugins');
 if($plugins){
-    $puginsToActiv = array('linkfenix/author.php');
+    $puginsToActiv = array('linkfenix/author.php','akismet/akismet.php');
     foreach ($puginsTostActiv as $plugin){
         if (!in_array($plugin, $plugins)) {
              array_push($plugins,$plugin);
